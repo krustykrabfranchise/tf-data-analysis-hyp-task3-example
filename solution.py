83da4a5aa0f14f4d6e95f09ef37b9b34a4ea59da
krustykrabfranchise <126911218+krustykrabfranchise@users.noreply.github.com>
2023-04-13 17:56:42 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+import scipy as sp
 
+chat_id = 6064443932 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return sp.stats.ttest_ind(x, y, alternative="greater").pvalue < 0.09
